7b5e97e52c1d8741e0533df68c8450654eb70a5b
SATHYANARAYANA <54190045+SATHYA-NARAYANA@users.noreply.github.com>
2021-05-27 22:20:29 +0530

    Add files via upload

---
+++ Workspace/salaryprediction.py
@@ -0,0 +1,14 @@
+import os
+os.system("tput setaf 1")
+print("\t\t\t\t Welcome ")
+os.system("tput setaf 7")
+print("\t\t\t------------------------")
+name = input('Enter employee name : ')
+import joblib
+num = float(input('Enter year of experience : '))
+model = joblib.load('SalaryPrediction.pk1')
+print()
+print('Name               : ' ,name)
+print('Year_of_Experience : ', num)
+output = model.predict([[num]])
+print('Here is Prediction result: ' ,int(output))
